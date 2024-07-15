6707225d3b3bb2ff89803eff67d5ade2051474c3
Muncherxyz <19kortlanblocker@gmail.com>
2024-07-15 14:53:43 -0500

    [Plane Selection]

--- unity-ar_slingshot_game/Assets/Scripts/PlaneSelection.cs
+++ unity-ar_slingshot_game/Assets/Scripts/PlaneSelection.cs
@@ -1,30 +1,42 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundations;
+using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+//My directives above
 
-public class PlaneSelection : MonoBehaviour
+public class PlaneSelectionManager : MonoBehaviour
 {
-
     public ARRaycastManager raycastManager;
     public ARPlaneManager planeManager;
-
-    public GameObject strtBttn;
-    public GameObject gmeM
-    public void HideButton()
-    {
-        strtBttn.SetActive(false);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject startCanvas;
+    private ARPlane selectedPlane = null;
     void Update()
     {
-        
+        if (selectedPlane == null && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+                if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    ARRaycastHit hit = hits[0];
+                    selectedPlane = hit.trackable as ARPlane;
+
+                    // Activate the canvas
+                    startCanvas.SetActive(true);
+
+                    // Disable the ARPlaneManager and the visual representation of all planes
+                    planeManager.enabled = false;
+                    foreach (ARPlane plane in planeManager.trackables)
+                    {
+                        if (plane != selectedPlane)
+                        {
+                            plane.gameObject.SetActive(false);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
